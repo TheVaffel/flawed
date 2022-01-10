008445a06009c7050212667dc0caa0eb5df362fb
Håkon Flatval <hkon20@hotmail.com>
2022-01-10 22:00:20 +0100

    Add flawed_add_tests
    Hating CMake more and more by the minute

--- FlawedTest.cmake
+++ FlawedTest.cmake
@@ -21,37 +21,35 @@ function(flawed_add_test FILE_NAME)
 
   target_link_libraries(${TEST_NAME} flawed_testing flawed)
 
-  message(">>>> " ${TEST_NAME})
-
-  get_target_property(TARGET_NAME ${TEST_NAME} NAME)
-
-  message("Target name = " ${TARGET_NAME})
-
-
-  # list(APPEND _FLAWED_TEST_LIST ${TEST_NAME})
-
-  message("Flawed output path = " ${FLAWED_OUTPUT_PATH})
-
   # Append test with prepended path
   set(_FLAWED_TEST_LIST ${_FLAWED_TEST_LIST} ${TEST_NAME} PARENT_SCOPE)
   set(_FLAWED_PREFIXED_TEST_LIST ${_FLAWED_PREFIXED_TEST_LIST} ${FLAWED_OUTPUT_PATH}/${TEST_NAME} PARENT_SCOPE)
-
-  message(">>>>>" ${_FLAWED_TEST_LIST})
 
   set_target_properties(${TEST_NAME} PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${FLAWED_OUTPUT_PATH})
   set_target_properties(${TEST_NAME} PROPERTIES EXCLUDE_FROM_ALL true)
 
   get_target_property(OUTPUT_DIR ${TEST_NAME} RUNTIME_OUTPUT_DIRECTORY)
 
-  message("AAAHHHH" ${OUTPUT_DIR})
 endfunction()
+
+function(flawed_add_tests ...)
+  set(_FLAWED_TEST_LIST "")
+  set(_FLAWED_PREFIXED_TEST_LIST "")
+
+  foreach(TEST ${ARGV})
+    flawed_add_test(${TEST})
+  endforeach()
+
+  set(_FLAWED_TEST_LIST ${_FLAWED_TEST_LIST} PARENT_SCOPE)
+  set(_FLAWED_PREFIXED_TEST_LIST ${_FLAWED_PREFIXED_TEST_LIST} PARENT_SCOPE)
+endfunction()
+
 
 
 function(flawed_write_test_target)
 
   add_custom_target(flawed_test "python3" ${FLAWED_PATH}/flawed_run_tests.py ${_FLAWED_PREFIXED_TEST_LIST})
 
-  message(">>>" ${_FLAWED_TEST_LIST})
   add_dependencies(flawed_test ${_FLAWED_TEST_LIST})
 
 endfunction()
