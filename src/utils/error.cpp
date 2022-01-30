f4149d851b8bc6137d78b028583585984587cad8
Håkon Flatval <hkon20@hotmail.com>
2022-01-30 11:55:25 +0100

    Add some more _fl_getln in the mix

--- src/utils/error.cpp
+++ src/utils/error.cpp
@@ -7,7 +7,7 @@ namespace flawed {
     FlAssertionError::FlAssertionError(const std::string& str) : FlException(_assertion_failed_string + " " + str) { }
 
     void _report_internal_error(const std::string& str) {
-        std::cerr << _add_color("Encountered internal error:", _fl_bg_red) + " " + str;
+        std::cerr << _flawed_getln(_add_color("Encountered internal error:", _fl_bg_red) + " " + str) << std::endl;
         throw std::logic_error(str);
     }
 };
