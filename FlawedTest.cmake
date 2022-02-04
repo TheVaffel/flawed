09fa7da726f7e8718cecae87e80fd5ef4036b35b
Håkon Flatval <hkon20@hotmail.com>
2022-02-05 00:03:37 +0100

    Add support for optional util files

--- FlawedTest.cmake
+++ FlawedTest.cmake
@@ -13,6 +13,14 @@ endif()
 link_directories(${FLAWED_PATH}/lib)
 include_directories(${FLAWED_PATH}/include)
 
+
+# Takes files containing test util code (.cpp) as arguments
+function(flawed_add_test_utils ...)
+  set(_FLAWED_TEST_UTIL_LIST ${_FLAWED_TEST_UTIL_LIST} ${ARGV} PARENT_SCOPE)
+
+endfunction()
+
+
 function(flawed_add_test FILE_NAME)
 
   get_filename_component(STRIPPED_FILE_NAME ${FILE_NAME} NAME)
@@ -36,6 +44,7 @@ function(flawed_add_test FILE_NAME)
   add_dependencies(flawed_run_${TEST_NAME} ${TEST_NAME})
 endfunction()
 
+
 function(flawed_add_tests ...)
   set(_FLAWED_TEST_LIST "")
   set(_FLAWED_PREFIXED_TEST_LIST "")
@@ -49,11 +58,22 @@ function(flawed_add_tests ...)
 endfunction()
 
 
+# Takes libraries to link to tests as arguments
+function(flawed_write_test_target ...)
 
-function(flawed_write_test_target)
+  set(FLAWED_TEST_UTIL_LIB flawed_test_utils)
+  set(TMP_ARCHIVE ${CMAKE_ARCHIVE_OUTPUT_DIRECTORY})
+  set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${FLAWED_OUTPUT_PATH})
+
+  add_library(${FLAWED_TEST_UTIL_LIB} STATIC ${_FLAWED_TEST_UTIL_LIST})
+
+  foreach(TEST_TARGET ${_FLAWED_TEST_LIST})
+    target_link_libraries(${TEST_TARGET} ${ARGV} ${FLAWED_TEST_UTIL_LIB})
+  endforeach()
 
   add_custom_target(flawed_test "python3" ${FLAWED_PATH}/flawed_run_tests.py ${_FLAWED_PREFIXED_TEST_LIST})
 
   add_dependencies(flawed_test ${_FLAWED_TEST_LIST})
 
+  set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${TMP_ARCHIVE})
 endfunction()
